dc4dda5b3da6a84b00b9c76f6278942e18fda632
Iuri Magnago <iurimp91@gmail.com>
2021-06-22 21:06:20 -0300

    Pages' routes added

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,29 @@ import SignUpPage from "./SignUpPage";
 import CashFlowPage from "./CashFlowPage";
 import AddIncomePage from "./AddIncomePage";
 import AddExpensePage from "./AddExpensePage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
             <GlobalStyle />
-            <AddExpensePage />
-        </>
+            <Switch>
+                <Route path="/" exact>    
+                    <SignInPage />
+                </Route>
+                <Route path="/sign-up" exact>
+                    <SignUpPage />
+                </Route>
+                <Route path="/cash-flow" exact>    
+                    <CashFlowPage />
+                </Route>
+                <Route path="/income" exact>    
+                    <AddIncomePage />
+                </Route>
+                <Route path="/expense" exact>    
+                    <AddExpensePage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
