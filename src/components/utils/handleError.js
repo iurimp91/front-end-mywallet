a45b898923f736de2b9c538751a170acf1c8e848
Iuri Magnago <iurimp91@gmail.com>
2021-08-10 20:58:07 -0300

    fix: alert error when description exceeds 30 characters

--- src/components/utils/handleError.js
+++ src/components/utils/handleError.js
@@ -5,6 +5,10 @@ export default function handleError(error, history) {
 			"Você foi desligado pelo servidor, por favor, faça login novamente."
 		);
 		history.push("/");
+	} else if (error.response.status === 400) {
+		alert(
+			"Utilize no máximo 30 caracteres na descrição."
+		);
 	} else {
 		alert(
 			"Algo deu errado com sua requisição, por favor, tente novamente."
