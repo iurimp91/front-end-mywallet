8d1d0ecd5b8269154c5de514858b06594f3f618a
Iuri Magnago <iurimp91@gmail.com>
2021-08-10 17:26:35 -0300

    fix: eslint.js export type

--- src/components/AddIncomePage.js
+++ src/components/AddIncomePage.js
@@ -18,11 +18,13 @@ export default function AddIncomePage() {
 		e.preventDefault();
 		setDisabled(true);
 
-		const config = { headers: { Authorization: `Bearer ${user.token || localUser.token}` } };
+		const config = {
+			headers: { Authorization: `Bearer ${user.token || localUser.token}` },
+		};
 		const centsValue = value * 100;
 		const body = { value: centsValue, description, type: "income" };
 		const request = axios.post("http://localhost:4000/input", body, config);
-        
+
 		request.then(() => {
 			setDisabled(false);
 			setValue("");
@@ -30,14 +32,18 @@ export default function AddIncomePage() {
 			alert("Entrada salva!");
 		});
 
-		request.catch(error => {
+		request.catch((error) => {
 			setDisabled(false);
-			if(error.response.status === 401) {
+			if (error.response.status === 401) {
 				localStorage.removeItem("user");
-				alert("Você foi desligado pelo servidor, por favor, faça login novamente.");
+				alert(
+					"Você foi desligado pelo servidor, por favor, faça login novamente."
+				);
 				history.push("/");
 			} else {
-				alert("Algo deu errado com sua requisição, por favor, tente novamente.");
+				alert(
+					"Algo deu errado com sua requisição, por favor, tente novamente."
+				);
 			}
 		});
 	}
@@ -45,7 +51,7 @@ export default function AddIncomePage() {
 	function goBack() {
 		history.push("/cash-flow");
 	}
- 
+
 	return (
 		<InputBody>
 			<Header>
@@ -53,8 +59,25 @@ export default function AddIncomePage() {
 				<AiOutlineRollback className="icon" onClick={goBack} />
 			</Header>
 			<Form onSubmit={addIncome}>
-				<input disabled={disabled} type="number" step="0.01" min="0.01" max="20000000" placeholder="Valor" required onChange={(e) => setValue(e.target.value)} value={value} />
-				<input disabled={disabled} type="text" placeholder="Descrição" required onChange={(e) => setDescription(e.target.value)} value={description} />
+				<input
+					disabled={disabled}
+					type="number"
+					step="0.01"
+					min="0.01"
+					max="20000000"
+					placeholder="Valor"
+					required
+					onChange={(e) => setValue(e.target.value)}
+					value={value}
+				/>
+				<input
+					disabled={disabled}
+					type="text"
+					placeholder="Descrição"
+					required
+					onChange={(e) => setDescription(e.target.value)}
+					value={description}
+				/>
 				<button disabled={disabled}>Salvar entrada</button>
 			</Form>
 		</InputBody>
